105ee14f209137c8e98c15cacd923b19d43b9511
kankala manojkumar <kankalamanojkumar@gmail.com>
2026-08-31 20:29:36 +0530

    Delete supabase directory

--- supabase/migrations/20260831005343_add_career_image_columns.sql
+++
@@ -1,4 +0,0 @@
--- Add image and detail columns to careers table
-ALTER TABLE careers ADD COLUMN IF NOT EXISTS image_url text;
-ALTER TABLE careers ADD COLUMN IF NOT EXISTS long_description text;
-ALTER TABLE careers ADD COLUMN IF NOT EXISTS syllabus text;
